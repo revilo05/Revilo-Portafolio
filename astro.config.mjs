204713168ade6638ff89a74f0469e76e2b71bce7
Oliver Abreu Mateo <132733631+revilo05@users.noreply.github.com>
2026-07-12 21:39:05 -0400

    chore: configure Astro, React and Vercel

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  integrations: [react()],
+  site: 'https://example.com',
+});
